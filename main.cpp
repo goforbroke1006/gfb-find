12fa92dc3bcb815d02ef9e02ed51c6f1a3a8ed8e
GoForBroke <go.for.broke1006@gmail.com>
2018-09-15 18:49:36 +0300

    Finish search by name

--- main.cpp
+++ main.cpp
@@ -6,70 +6,83 @@
 #include <boost/filesystem.hpp>
 #include <boost/range/iterator_range.hpp>
 #include <boost/regex.hpp>
+#include <chrono>
 
+#define ARGS_ERROR_MSG "Wrong args number. Usage: gfb_find /target/dir ([a-z0-9]{4})\\.txt"
 
 using namespace std;
+using namespace std::chrono;
 
 namespace fs = boost::filesystem;
 
-//
+bool match_file_name(const fs::path &_path, const boost::regex &regex) {
+    static boost::smatch what;
+    const string &filename = _path.filename().string();
+    return boost::regex_match(filename, what, regex);
+}
 
-void search_routine(fs::path p, const char *pattern) {
-//    std::cout << "Thread number: " << number << std::endl;
-
-    const boost::regex my_filter(pattern);
-    boost::filesystem::directory_iterator it(p), end_itr;
+void search_routine(const fs::path &path, const boost::regex &regex) {
+    fs::recursive_directory_iterator it(path), end_itr;
     for (; it != end_itr; ++it) {
         if (!fs::is_regular_file(it->status())) continue;
-        boost::smatch what;
-        const string &filename = it->path().filename().string();
-        if (!boost::regex_match(filename, what, my_filter)) continue;
-
+        if (!match_file_name(it->path(), regex)) continue;
         std::cout << it->path().string() << std::endl;
     }
 }
 
 int main(int argc, char **argv) {
-
-    if (argc == 1) {
-        cerr << "Wrong args number. Usage: gfb_find /target/dir ([a-z0-9]{4})\\.txt log" << endl;
+    if (argc == 1 || argc > 3) {
+        cerr << ARGS_ERROR_MSG << endl;
+        return -1;
     }
 
+    fs::path progLocation(argv[0]);
+    string progDir = progLocation.parent_path().string();
+
+    fstream LOGGER;
+    LOGGER.open(progDir + "/gfb_find.log", fstream::out);
+
     string targetDir;
-    vector<string> patterns;
+    string pattern;
 
     if (argc == 2) {
-        targetDir = argv[0]; // TODO: get parent dir
-        patterns.emplace_back(argv[1]);
-    } else if (argc >= 3) {
+        targetDir = progDir;
+        pattern = argv[1];
+    } else if (argc == 3) {
         targetDir = argv[1];
-        for (int i = 2; i < argc; ++i) {
-            patterns.emplace_back(argv[i]);
+        pattern = argv[2];
+    }
+
+    LOGGER << "Target dir : " << targetDir << endl;
+    LOGGER << "Pattern : " << pattern << endl;
+
+    milliseconds beforeExec = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+
+    const boost::regex my_filter(pattern);
+    std::vector<std::thread> ts;
+
+    fs::directory_iterator it(targetDir), end_itr;
+    for (; it != end_itr; ++it) {
+        if (fs::is_regular_file(it->status())) {
+            if (match_file_name(it->path(), my_filter))
+                std::cout << it->path().string() << std::endl;
+        } else {
+//            search_routine(it->path(), my_filter);
+            std::thread t = std::thread(&search_routine, it->path(), my_filter);
+            ts.push_back(std::move(t));
         }
     }
 
-//    fs::directory_iterator it{targetDir}, end;
-//    for (; it != end; ++it)
-//        cout << it->path() << endl;
+    LOGGER << "Used routines : " << ts.size() << endl;
+    for (auto &t : ts) {
+        t.join();
+    }
 
-    search_routine(fs::path("/home/goforbroke/scripts"), ".*\\.sh");
+    milliseconds afterExec = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
 
-//    cout << argv[0] << endl;
-//    cout << argv[1] << endl;
-//    cout << argv[2] << endl;
+    LOGGER << "Spend time : " << (afterExec - beforeExec).count() << " milliseconds" << endl;
 
-//    unsigned int concurrentNum = std::thread::hardware_concurrency() * 2;
-//
-//    const string path = "/home/goforbroke/";
-//    const fs::directory_iterator &range = fs::directory_iterator(path);
-//
-//    std::vector<std::thread> ts;
-//    for (unsigned int i = 0; i < concurrentNum; ++i) {
-//        std::thread t = std::thread(&search_routine, i, range);
-//        ts.push_back(std::move(t));
-//    }
-//    for (auto &t : ts) {
-//        t.join();
-//    }
+    LOGGER.close();
+
     return 0;
 }
